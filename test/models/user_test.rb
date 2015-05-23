bd3a3581890c698f45e74bc75bda7e430e2652c6
flutesandyou <flutesandyou@gmail.com>
2015-05-23 00:15:53 -0700

    user controller fix conflict

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,11 +18,6 @@ class UserTest < ActiveSupport::TestCase
 		assert_not @user.valid?
 	end
 
-	test "email should be present" do
-		@user.email = "    "
-		assert_not @user.valid?
-	end
-
 	test "name should not be too long" do
 		@user.name = "a" * 51
 		assert_not @user.valid?
@@ -34,13 +29,12 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "email validation should accept valid addresses" do
-
-		valid_addresses = %w(user@example.com USER@foo.COM A_US-ER@foo.bar.org
-												 first.last@foo.jp alice+bob@baz.cn)
-		valid addresses.each do |valid_address|
-
+		valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+												 first.last@foo.jp alice+bob@baz.cn]
+		valid_addresses.each do |valid_address|
 			@user.email = valid_address
 			assert @user.valid?, "#{valid_address.inspect} should be valid"
+		end
 	end
 
 	test "email validation should reject invalid addresses" do
@@ -48,7 +42,8 @@ class UserTest < ActiveSupport::TestCase
 													 foo@bar_baz.com foo@bar+baz.com]
 		invalid_addresses.each do |invalid_address|
 			@user.email = invalid_address
-			assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"	  
+			assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+		end
 	end
 
 	test "email addresses should be unique" do
@@ -57,10 +52,9 @@ class UserTest < ActiveSupport::TestCase
 		@user.save
 		assert_not duplicate_user.valid?
 	end
-	
+
 	test "password should have a minimum length" do
 		@user.password = @user.password_confirmation = "a" * 5
 		assert_not @user.valid?
 	end
 end
-
