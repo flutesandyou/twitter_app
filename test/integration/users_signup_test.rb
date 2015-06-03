75daac143e04aa3d919b800f70d1d627b8f66ea4
flutesandyou <flutesandyou@gmail.com>
2015-06-02 21:41:03 -0700

    Finishing Sign-up-exercises

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,6 +13,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 		                           password_confirmation: "bar" }
 		end
 	assert_template 'users/new'
+	assert_select 'div#<CSS id for error explanation>'
+	assert_select 'div.<CSS class for field with error>'
 	end
 
 	test "valid signup information" do
@@ -23,6 +25,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 																					password:              "password",
 																					password_confirmation: "password"}
 		end
-	assert_template 'users/show'
+		assert_template 'users/show'
+		assert_not flash.nil?
 	end
 end
