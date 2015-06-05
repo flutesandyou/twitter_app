f2df93a2276c2bdd083caef851728f317efac3a6
flutesandyou <flutesandyou@gmail.com>
2015-06-05 07:17:58 -0700

    Finish log in/log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -32,7 +32,15 @@ module SessionsHelper
 
 	# Logs out the current user.
 	def log_out
+		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
+	end
+	
+	# Forgets a persistent session.
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
 	end
 end
