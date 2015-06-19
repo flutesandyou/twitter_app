80903ec05c8db640b4b72a8630fe76b5f87a77f1
flutesandyou <flutesandyou@gmail.com>
2015-06-19 23:12:20 +1000

    finishing chapter 10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 FirstApp::Application.routes.draw do
   
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   root 'static_pages#home'
@@ -15,6 +19,7 @@ FirstApp::Application.routes.draw do
   resources :microposts
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
