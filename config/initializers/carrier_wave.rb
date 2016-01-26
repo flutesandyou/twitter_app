b84d6705fd7287e7bb10c0376fdbd45fe32bd0d0
flutesandyou <flutesandyou@gmail.com>
2016-01-26 06:11:05 -0800

    Trying to fix Carrierwave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,8 +4,14 @@ if Rails.env.production?
 			# Configuration for Amazon S3
 			:provider 							=> 'AWS',
 			:aws_access_key_id			=> ENV['S3_ACCESS_KEY'],
-			:aws_secret_access_key	=> ENV['S3_SECRET_KEY']
+			:aws_secret_access_key	=> ENV['S3_SECRET_KEY'],
+			:region                 => 'eu-west-2',                  # optional, defaults to 'us-east-1'
+      :host                   => 's3.example.com',             # optional, defaults to nil
+      :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+
 		}
 		config.fog_directory			=  ENV['S3_BUCKET']
+		config.fog_public    		  = false                                   # optional, defaults to true
+    config.fog_attributes     = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 	end
 end
