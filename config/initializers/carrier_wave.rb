a9b4d4e80b878ee1afa18b8988a66c980c6de5be
flutesandyou <flutesandyou@gmail.com>
2016-01-26 06:38:38 -0800

    Trying to make S3 work

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,14 +4,8 @@ if Rails.env.production?
 			# Configuration for Amazon S3
 			:provider 							=> 'AWS',
 			:aws_access_key_id			=> ENV['S3_ACCESS_KEY'],
-			:aws_secret_access_key	=> ENV['S3_SECRET_KEY'],
-			:region                 => 'eu-west-2',                  # optional, defaults to 'us-east-1'
-      :host                   => 's3.example.com',             # optional, defaults to nil
-      :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
-
+			:aws_secret_access_key	=> ENV['S3_SECRET_KEY']               # optional, defaults to 'us-east-1'
 		}
-		config.fog_directory			=  ENV['S3_BUCKET']
-		config.fog_public    		  = false                                   # optional, defaults to true
-    config.fog_attributes     = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+		config.fog_directory			= ENV['S3_BUCKET']
 	end
 end
